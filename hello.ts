26b4bfee019ad850883c3544b476d47479ead6f5
Amine <aminbouali1995@hotmail.com>
2021-02-10 17:36:57 +0100

    Testing project created

---
+++ hello.ts
@@ -0,0 +1,27 @@
+export class helloer{
+    sayHello(){
+        console.log("hello")
+    }
+    amin='amin';
+    constructor() {
+    }
+    anothermethod(){
+
+
+
+
+    }
+    methodWithParams(name:string){
+
+    }
+    methodWithParamsAndReturn(name:string,anotherParam:any):any{
+        return null;
+    }
+    methodWithSomeInstructions(param1:any,param2:any){
+        console.log("hi");
+        console.log("Testing");
+        console.log("end");
+    }
+}
+export class aSimpleEmptyClass{
+}
